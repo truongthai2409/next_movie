b3cc46404232a1cc98505e0b2e16d16412c12116
Truong Thai <52000714@student.tdtu.edu.vn>
2025-03-18 00:08:11 +0700

    change background

--- src/components/ui/trailer/youtube.tsx
+++ src/components/ui/trailer/youtube.tsx
@@ -5,25 +5,27 @@ import Image from "next/image";
 
 const YouTubeVideo = ({ trailerUrl, posterUrl, name }: {trailerUrl: string, posterUrl: string, name: string}) => {
   const [showImage, setShowImage] = useState(false);
-  const [isMuted, setIsMuted] = useState(true); // Trạng thái âm thanh
+  const [isMuted, setIsMuted] = useState(true);
   const [player, setPlayer] = useState<YT.Player | null>(null);
+  
 
   const handleVideoEnd = () => {
     setShowImage(true);
   };
+  
   const toggleMute = () => {
     if (player) {
       if (isMuted) {
-        player.unMute(); // Bật âm thanh
+        player.unMute();
       } else {
-        player.mute(); // Tắt âm thanh
+        player.mute();
       }
-      setIsMuted(!isMuted); // Cập nhật trạng thái
+      setIsMuted(!isMuted);
     }
   };
 
   return (
-    <div className="relative w-full h-[500px]">
+    <div className="relative w-full h-[250px] lg:h-[500px]">
       {!showImage && trailerUrl ? (
         <div>
           <YouTube
@@ -40,14 +42,14 @@ const YouTubeVideo = ({ trailerUrl, posterUrl, name }: {trailerUrl: string, post
             onReady={(event) => setPlayer(event.target)}
             onStateChange={(event) => {
               if (event.data === 0) {
-                handleVideoEnd(); // Gọi khi video kết thúc
+                handleVideoEnd();
               }
             }}
             className="absolute w-full h-full"
           />
           <button
             onClick={toggleMute}
-            className="absolute z-30 bottom-2 right-16 bg-black bg-opacity-50 text-white px-3 py-1 rounded-md"
+            className="absolute z-30 bottom-2 right-4 lg:right-16 bg-black bg-opacity-50 text-white px-3 py-1 rounded-md"
           >
             {isMuted ? "Bật âm thanh" : "Tắt âm thanh"}
           </button>
@@ -64,7 +66,7 @@ const YouTubeVideo = ({ trailerUrl, posterUrl, name }: {trailerUrl: string, post
   );
 };
 
-// Helper function để lấy video ID từ URL YouTube
+// Helper function to get video ID from YouTube URL
 const getYouTubeVideoId = (url: string) => {
   const regExp =
     /(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/;
